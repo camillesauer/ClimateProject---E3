45504a796822827aae1ad20391587fbe7eaed338
camillesauer <camille-sauer@hotmail.fr>
2022-02-13 11:57:22 +0100

    ok

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -4,6 +4,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from flask_wtf.file import FileField
 from wtforms.validators import DataRequired
+from ..models import Role, Img
+from wtforms_sqlalchemy.fields import QuerySelectField
 
 class ImgForm(FlaskForm):
     """
@@ -11,4 +13,25 @@ class ImgForm(FlaskForm):
     """
     name = StringField('Name', validators=[DataRequired()])
     file = FileField()
+    submit = SubmitField('Submit')
+
+
+class RoleForm(FlaskForm):
+    """
+    Form for admin to add or edit a role
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# app/admin/forms.py
+
+class UserAssignForm(FlaskForm):
+    """
+    Form for admin to assign departments and roles to employees
+    """
+    img = QuerySelectField(query_factory=lambda: Img.query.all(),
+                                  get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name")
     submit = SubmitField('Submit')
