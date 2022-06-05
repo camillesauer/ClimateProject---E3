83e8c1c2675bee09decdba55697489d59c92ecf1
camillesauer <camille-sauer@hotmail.fr>
2022-06-05 13:16:17 +0200

    ok

--- tests_algo.py
+++ tests_algo.py
@@ -2,20 +2,56 @@
 import unittest
 from flask_testing import TestCase
 from model.load import predict
-from app import create_app
+from app import create_app, db
 from werkzeug.utils import secure_filename
 from app.admin.forms import ImgForm
+from app.models import User
 import os
 
 class TestAlgo(TestCase):
 
     def create_app(self):
+
+        # pass in test configurations
         config_name = 'testing'
         app = create_app(config_name)
         app.config.update(
             SQLALCHEMY_DATABASE_URI='mysql+pymysql://student:PanapoiC19!@localhost/climatedb'
         )
         return app
+
+    def setUp(self):
+        """
+        Will be called before every test
+        """
+
+        db.create_all()
+
+        # create test admin user
+        admin = User()
+        admin.username = "admin2"
+        admin.password = "admin2016"
+        admin.is_admin = True
+
+        # create test non-admin user
+        user = User()
+        user.username = "test_user"
+        user.password = "test2016"
+
+        # save users to database
+        db.session.add(admin)
+        db.session.add(user)
+        db.session.commit()
+
+    def tearDown(self):
+        """
+        Will be called after every test
+        """
+
+        db.session.remove()
+        db.drop_all()
+
+    # tests.py
 
     def test_prediction_algo(self):
 
