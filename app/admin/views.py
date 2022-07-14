5a18f333b00b67ff22f30690e03d32fa81031229
camillesauer <camille-sauer@hotmail.fr>
2022-07-14 22:59:52 +0200

    ok

--- app/admin/views.py
+++ app/admin/views.py
@@ -45,7 +45,7 @@ def add_img():
         app = Flask(__name__, instance_relative_config=True)
         filename = secure_filename(form.file.data.filename)
         root_path = os.path.dirname(os.path.abspath(__file__))
-        app.config['UPLOAD_FOLDER'] = os.path.join(root_path, 'static/uploads')
+        app.config['UPLOAD_FOLDER'] = os.path.join('./static/uploads')
         form.file.data.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
         prediction = predict(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         img = Img(name=filename, user_id=current_user.id, img=filename, mimetype=form.file.data.mimetype, prediction=prediction[0], out=prediction[1])
