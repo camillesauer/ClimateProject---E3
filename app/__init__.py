b6a9cac84a35a3c700ae32705c95dd5bb14923ea
camillesauer <camille-sauer@hotmail.fr>
2022-07-14 16:25:53 +0200

    ok

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask import abort, Flask, render_template
-import os
+import psycopg2
 # local imports
 
 db = SQLAlchemy()
