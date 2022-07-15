f8e62bdd2260f6c1f62e350b93077c3b1da4099a
camillesauer <camille-sauer@hotmail.fr>
2022-07-15 16:37:51 +0200

    ok

--- app/admin/views.py
+++ app/admin/views.py
@@ -45,10 +45,9 @@ def add_img():
     if form.validate_on_submit():
         app = Flask(__name__, instance_relative_config=True)
         filename = secure_filename(form.file.data.filename)
-        UPLOAD_FOLDER = '/uploads'
+        UPLOAD_FOLDER = 'app/app/admin/uploads'
         app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        form.file.data.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
+        form.file.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         prediction = predict(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         img = Img(name=filename, user_id=current_user.id, img=filename, mimetype=form.file.data.mimetype, prediction=prediction[0], out=prediction[1])
         db.session.add(img)
