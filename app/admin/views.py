d2498971f92ec8428a526e41b440573005a7cdf3
camillesauer <camille-sauer@hotmail.fr>
2022-07-15 16:26:54 +0200

    ok

--- app/admin/views.py
+++ app/admin/views.py
@@ -47,9 +47,8 @@ def add_img():
         filename = secure_filename(form.file.data.filename)
         UPLOAD_FOLDER = '/uploads'
         app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-        tbasedir = os.path.abspath(os.path.dirname(__file__))
-        print(tbasedir)
-        form.file.data.save(os.path.join(tbasedir, app.config['UPLOAD_FOLDER'], filename))
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        form.file.data.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
         prediction = predict(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         img = Img(name=filename, user_id=current_user.id, img=filename, mimetype=form.file.data.mimetype, prediction=prediction[0], out=prediction[1])
         db.session.add(img)
